8d931bf2815f8299ff8cfdaf05460ad2982d85a6
Ashish <ashishteddy2@gmail.com>
2019-12-05 09:20:23 +0530

    done with test

--- test/day_45_morning_challenge_test.dart
+++ test/day_45_morning_challenge_test.dart
@@ -1,6 +1,8 @@
 import 'package:test/test.dart';
-
+import '../bin/main.dart';
 void main() {
   test('Test Case 1', () {
+    expect(stockProfit([7,1,5,3,6,4]), 5);
+    expect(stockProfit([7,4,5,10,6,12]), 8)
   });
 }
