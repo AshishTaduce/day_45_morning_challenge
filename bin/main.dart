acba911a2d73723343dc0f9c963fd57eb6831d84
Ashish <ashishteddy2@gmail.com>
2019-12-05 09:17:39 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,6 @@
 // Best Time to Buy and Sell Stock
+import 'dart:math';
+
 /// Say you have an array for which the ith element is the price of a given stock on day i.
 //  If you were only permitted to complete at most one transaction (i.e., buy one
 // and sell one share of the stock), design an algorithm to find the maximum profit.
@@ -7,6 +9,16 @@
 //  Input: [7,1,5,3,6,4]
 //  Output: 5
 
+stockProfit(List <int> stock){
+  int maxProfit = 0;
+  for (int i = 0; i < stock.length; i++){
+    int profit = stock.sublist(i, stock.length).reduce(max) - stock [i];
+    if (profit > maxProfit){
+      maxProfit = profit;
+    }}
+  return maxProfit;
+}
 
 main() {
+  print(stockProfit([7,1,5,3,6,4]));
 }
